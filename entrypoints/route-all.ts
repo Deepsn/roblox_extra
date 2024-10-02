1c2107a137ee1c54cb46ccd1c38566d165828701
Deepsn <75620590+Deepsn@users.noreply.github.com>
2024-10-01 22:09:33 -0300

    feat: add all react hooks to global

--- entrypoints/route-all.ts
+++ entrypoints/route-all.ts
@@ -1,4 +1,4 @@
-import { hookReact } from "@/utils/react/hooks";
+import { hookReact } from "@/utils/react/hook-react";
 import { waitForObject } from "@/utils/wait-for-object";
 
 export default defineUnlistedScript(async () => {
@@ -11,8 +11,32 @@ export default defineUnlistedScript(async () => {
 
 	await waitForObject(window, "React");
 
-	hookReact();
-
 	console.log("disabled warning");
 	Roblox.DeveloperConsoleWarning.showWarning = () => {};
+
+	// React global utils
+	const internals = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+	const dispatcher = internals.ReactCurrentDispatcher;
+	let current = dispatcher.current;
+
+	Object.defineProperty(dispatcher, "current", {
+		enumerable: true,
+		set(value) {
+			current = value;
+
+			if (!value) return;
+
+			const keys = Object.keys(value).filter((key) => key.includes("use"));
+
+			for (const key of keys) {
+				if (Reflect.has(window, key)) continue;
+				Reflect.set(window, key, (React as any)[key]);
+			}
+		},
+		get() {
+			return current;
+		},
+	});
+
+	hookReact();
 });
--- types/global-window.d.ts
+++ types/global-window.d.ts
@@ -1,7 +1,12 @@
+import type { ConstructorHook } from "@/utils/react/types/hook";
+
 export declare global {
 	const Roblox: Roblox;
-	const React: React;
+	const React: typeof import("react") & {
+		__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: any;
+	};
 	const RobloxExtra: RobloxExtra;
+	const useEffect: typeof import("react").useEffect;
 
 	interface Window {
 		RobloxThumbnails: RobloxThumbnails;
@@ -9,6 +14,7 @@ export declare global {
 		RobloxExtra: RobloxExtra;
 		React: typeof import("react");
 		ReactDOM: typeof import("react-dom");
+		useEffect: typeof import("react").useEffect;
 	}
 
 	interface RobloxExtra {
