01c7ad29392f3a528035dab2222de5e4fe5d43cc
Deepsn <75620590+Deepsn@users.noreply.github.com>
2025-04-23 10:55:20 -0300

    fix: rename explorer parser package to the npm published one

--- utils/explorer/download-asset.ts
+++ utils/explorer/download-asset.ts
@@ -1,5 +1,5 @@
 import { Instance } from "@/utils/explorer/instance";
-import { parse_asset } from "explorer-parser-wasm";
+import { parse_asset } from "r-extra-explorer-parser-wasm";
 
 export async function downloadAsset(assetId: string): Promise<Instance> {
 	const instanceData = await parse_asset(assetId);
