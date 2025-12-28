87df8dc35680da7770156b40bb37497bdec795a9
Deepsn <75620590+Deepsn@users.noreply.github.com>
2025-12-28 13:41:20 -0300

    feat: hook jsx function on createElement hook

--- src/types/global-window.d.ts
+++ src/types/global-window.d.ts
@@ -10,6 +10,8 @@ export declare global {
 		__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: any;
 	};
 
+	const ReactJSX: typeof import("react/jsx-runtime");
+
 	const ReactStyleGuide: _RobloxStyleGuide;
 	const ReactUtilities: _ReactUtilities;
 
--- src/utils/react/hook-react.ts
+++ src/utils/react/hook-react.ts
@@ -1,22 +1,36 @@
 import type { ReactElement } from "react";
+import { Logger } from "@/utils/helpers/logger";
 import { hookFunction } from "@/utils/hook-function";
 import { onCreateElement } from "@/utils/react/on-create-element";
 
+const logger = new Logger("ReactHook", "#61DAFB");
+
 export function hookReact() {
-	hookFunction(React, "createElement", (createElement, react, args) => {
+	const hookCreateElement = (
+		createElement: (typeof React)["createElement"] | (typeof ReactJSX)["jsx"] | (typeof ReactJSX)["jsxs"],
+		react: typeof React | typeof ReactJSX,
+		args:
+			| Parameters<(typeof React)["createElement"]>
+			| Parameters<(typeof ReactJSX)["jsx"]>
+			| Parameters<(typeof ReactJSX)["jsxs"]>,
+	) => {
 		const result = Reflect.apply(createElement, react, args) as ReactElement;
 
 		try {
+			logger.debug("createElement", args[1] ?? args[0]);
 			const type = result.type;
 			const proxy = onCreateElement(type, result.props);
-
 			if (proxy) {
 				result.type = proxy;
 			}
 		} catch (err) {
-			console.error(err);
+			logger.error(err);
 		}
 
 		return result;
-	});
+	};
+
+	hookFunction(React, "createElement", hookCreateElement);
+	hookFunction(ReactJSX, "jsx", hookCreateElement);
+	hookFunction(ReactJSX, "jsxs", hookCreateElement);
 }
